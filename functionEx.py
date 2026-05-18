2428d68d948a3cbdd9d6d9bc09dd9db2aaecf0ec
ShubhamShrivastav0702 <shrivastavshubhamji1994@gmail.com>
2026-05-18 15:54:05 +0530

    function

--- functionEx.py
+++ functionEx.py
@@ -19,7 +19,7 @@ function have meaning full name
 def function():
     //code
     
-    
+function()
 if we want to use function we call function using 
 function name
 
@@ -41,16 +41,110 @@ user define
 
 create multiple part of a single one program
 
+BasicFunction
+
+
 
 
 """
 
-def messagefunction():
-    print("hello Message")
+# def  abc():
+#     print("hello")
+    
+
+# for x in range(5):
+#     abc()   
+    
+    
+    
+"""
+function type->
+
+no return no args   add profile
+no return with args    for database data save 
+return witn no args  use-  ins/car/rto
+return with  args
 
 
-messagefunction() # function calling
+"""
 
-print("1")
-print("2")
-print("3")
+# no return
+# def message():
+#     print("hello")
+    
+    
+# val= message()
+# print(val)
+
+
+
+# def message(fname):
+#     print(f"hello {fname}")
+    
+    
+# message("raj")
+# message("mohan")
+# message("anuj")
+
+# # no return with args
+# def add(val1,val2):
+#     res=val1+val2
+#     # print(res)
+
+# add(3,6)
+# add(4,9)
+
+
+
+# no args with return
+# def msg():
+#     return "hy i am user"
+
+# val=msg()
+# print(val)
+
+
+
+# def sum(a,b):
+#     res=a+b
+#     return res
+
+# ans= sum(3,6)
+# print(ans)
+
+
+
+
+
+
+
+
+
+
+
+# def calculateprofit(amount,month):
+#      emi = amount/month
+#      for x in range(1,month+1):    
+#         finalemi = emi+ emi*0.02
+#         print(finalemi)
+    
+# calculateprofit(20000,12)
+
+# calculateprofit(30000,6)
+
+
+
+
+
+# default value
+
+
+def info(fname=None,age=None,address="Indore"):
+    print(f"firstName : {fname}\nage : {age}\naddress : {address}")
+
+# position base
+# info("shubham",23)
+# info(23,"amit","ujjain")
+# info(age=23,address="ujjian" ,fname="mohan")#key and value pair
+
+# advance function type pending 
